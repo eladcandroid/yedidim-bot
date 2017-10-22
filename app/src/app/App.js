6295f5cc3b950145b4e8273824f7a6167f001158
yossipik <yossi.pik@gmail.com>
2017-10-22 12:21:46 +0300

    Integrate Sentry to app

--- app/src/app/App.js
+++ app/src/app/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import Sentry from 'sentry-expo';
 import configureStore from '../store/configureStore';
 import HomeScreen from "../components/HomeScreen";
+
+Sentry.config('https://757d9938f3b64f20a85d536879e30e33@sentry.io/233665').install();
 
 const store = configureStore;
 
