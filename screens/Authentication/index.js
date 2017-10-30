51d43de98d3b38b0fcef070b27603335e922df86
Alan Rubin <alan.rubin@keytree.co.uk>
2017-10-30 09:40:37 +0200

    Basic redirect flow

--- screens/Authentication/index.js
+++ screens/Authentication/index.js
@@ -20,6 +20,7 @@ import {
 import { View, WebView } from "react-native";
 import styled from "styled-components/native";
 import signIn, { signOut } from "../../api";
+import { AuthSession } from "expo";
 
 const IntroText = styled.Text`
   text-align: center;
@@ -41,39 +42,21 @@ const StyledButton = styled(Button)`
 `;
 
 export default class AuthenticationScreen extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      phone: "",
-      code: "",
-      authenticated: false
-    };
-  }
+  state = {
+    result: null
+  };
+
+  handleAuthentication = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl();
+    let result = await AuthSession.startAsync({
+      authUrl:
+        `https://yedidim-sandbox-2.firebaseapp.com/?` +
+        `redirect_uri=${encodeURIComponent(redirectUrl)}`
+    });
+    this.setState({ result });
+  };
+
   render() {
-    const { authenticated, phone, code } = this.state;
-
-    const canSubmit = authenticated ? !!code : !!phone;
-
-    const patchPostMessageFunction = function() {
-      var originalPostMessage = window.postMessage;
-
-      var patchedPostMessage = function(message, targetOrigin, transfer) {
-        originalPostMessage(message, targetOrigin, transfer);
-      };
-
-      patchedPostMessage.toString = function() {
-        return String(Object.hasOwnProperty).replace(
-          "hasOwnProperty",
-          "postMessage"
-        );
-      };
-
-      window.postMessage = patchedPostMessage;
-    };
-
-    const patchPostMessageJsCode =
-      "(" + String(patchPostMessageFunction) + ")();";
-
     return (
       <Container>
         <Header>
@@ -84,71 +67,19 @@ export default class AuthenticationScreen extends React.Component {
           <Right />
         </Header>
         <Content padder>
-          <IntroText>
-            Please enter your phone number to receive a code by SMS for
-            authentication so to be able to accept events.
-          </IntroText>
-          <Form>
-            <Item>
-              <Input
-                value={phone}
-                keyboardType="numeric"
-                disabled={authenticated}
-                style={{
-                  color: authenticated ? "#d2d4d8" : "black"
-                }}
-                onChangeText={phone => this.setState({ phone })}
-                placeholder="Phone number"
-              />
-            </Item>
-            {authenticated && (
-              <Item>
-                <Input
-                  value={code}
-                  onChangeText={code => this.setState({ code })}
-                  keyboardType="numeric"
-                  placeholder="Enter code received"
-                />
-              </Item>
-            )}
-          </Form>
+          {this.state.result ? (
+            <Text>{JSON.stringify(this.state.result)}</Text>
+          ) : (
+            <IntroText>
+              You are not authenticated yet. Please authenticate to receive
+              events.
+            </IntroText>
+          )}
           <ButtonsView>
-            <StyledButton
-              disabled={!canSubmit}
-              success={canSubmit}
-              onPress={() => {
-                this.setState({ authenticated: true });
-                this.webview.postMessage(phone);
-                // signIn();
-              }}
-            >
-              <Text>
-                {authenticated ? "Verify Code" : "Send me the code by SMS"}
-              </Text>
+            <StyledButton success onPress={this.handleAuthentication}>
+              <Text>Authenticate me</Text>
             </StyledButton>
-            {authenticated && (
-              <StyledButton
-                danger
-                onPress={() => {
-                  this.setState({ authenticated: false, phone: "" });
-                  // signOut();
-                }}
-              >
-                <Text>Start Again</Text>
-              </StyledButton>
-            )}
           </ButtonsView>
-          <WebView
-            ref={webview => (this.webview = webview)}
-            mixedContentMode="always"
-            injectedJavaScript={patchPostMessageJsCode}
-            style={{ height: 500, width: 500 }}
-            source={require("./webview.html")}
-            onMessage={event => {
-              console.log(">>>onMessage:", event.nativeEvent.data);
-              signIn(JSON.parse(event.nativeEvent.data));
-            }}
-          />
         </Content>
       </Container>
     );
