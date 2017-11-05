08b8051d3636bac4a625f0838a10dead26fedc20
Alan Rubin <alan.rubin@keytree.co.uk>
2017-11-05 11:07:01 +0200

    Formatting and fixing eslint issues

--- screens/Authentication/index.js
+++ screens/Authentication/index.js
@@ -1,84 +1,77 @@
-import React from "react";
+import React from 'react'
 import {
   Button,
   Text,
   Container,
-  Card,
-  CardItem,
   Body,
   Content,
   Header,
   Title,
   Left,
-  Icon,
-  Right,
-  Form,
-  Item,
-  Input,
-  Label
-} from "native-base";
-import { View, ActivityIndicator } from "react-native";
-import styled from "styled-components/native";
-import { signIn, loggedInUser, signOut } from "../../api";
-import { AuthSession } from "expo";
+  Right
+} from 'native-base'
+import { ActivityIndicator } from 'react-native'
+import styled from 'styled-components/native'
+import { AuthSession } from 'expo'
+import { signIn, loggedInUser, signOut } from '../../api'
 
 const IntroText = styled.Text`
   text-align: center;
   font-weight: bold;
   margin: 30px 10px 30px;
   font-size: 16px;
-`;
+`
 
 const ButtonsView = styled.View`
   flex-direction: row;
   margin-top: 40px;
-`;
+`
 
 const StyledButton = styled(Button)`
   flex-grow: 1;
   align-content: center;
   justify-content: center;
   margin: 0 5px;
-`;
+`
 
 export default class AuthenticationScreen extends React.Component {
   state = {
     user: null,
     loading: true
-  };
+  }
 
   async componentWillMount() {
-    const user = await loggedInUser();
-    this.setState({ user, loading: false });
+    const user = await loggedInUser()
+    this.setState({ user, loading: false })
   }
 
   handleAuthentication = async () => {
-    this.setState({ error: undefined });
+    this.setState({ error: undefined })
 
-    const redirectUrl = AuthSession.getRedirectUrl();
+    const redirectUrl = AuthSession.getRedirectUrl()
     const result = await AuthSession.startAsync({
       authUrl:
         `https://yedidim-sandbox-2.firebaseapp.com/?` +
         `redirect_uri=${encodeURIComponent(redirectUrl)}` +
         `&language=he`
-    });
+    })
 
-    const { error, verificationId, code } = result.params;
+    const { error, verificationId, code } = result.params
 
     if (!error && verificationId && code) {
       signIn(verificationId, code)
         .then(user => this.setState({ user }))
-        .catch(error => this.setState({ error }));
+        .catch(errorThrown => this.setState({ error: errorThrown }))
     }
-  };
+  }
 
   handleLogout = async () => {
-    await signOut();
-    this.setState({ user: undefined });
-  };
+    await signOut()
+    this.setState({ user: undefined })
+  }
 
   render() {
-    const { user, loading, error } = this.state;
+    const { user, loading, error } = this.state
 
     return (
       <Container>
@@ -119,6 +112,6 @@ export default class AuthenticationScreen extends React.Component {
           </Content>
         )}
       </Container>
-    );
+    )
   }
 }
