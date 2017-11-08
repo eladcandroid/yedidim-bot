a838c7b0f020e8a3adfafa08b141e3b52efe6fda
yossipik <yossi.pik@gmail.com>
2017-11-08 13:38:34 +0200

    Bot - fallback to open street map in case of geocoding failure

--- functions/geocoder.js
+++ functions/geocoder.js
@@ -1,21 +1,29 @@
 const NodeGeocoder = require('node-geocoder');
 
-let nodeGeocoder;
+let googleGeocoder;
+let osmGeocoder;
 
 module.exports = {
   init: function(apiKey){
-    const options = {
+    const googleOptions = {
       provider: 'google',
       httpAdapter: 'https',
       language: 'iw',
       apiKey: apiKey,
       formatter: null
     };
-    nodeGeocoder = NodeGeocoder(options);
+    googleGeocoder = NodeGeocoder(googleOptions);
+    const osmOptions = {
+      provider: 'openstreetmap',
+      httpAdapter: 'https',
+      language: 'he',
+      formatter: null
+    };
+    osmGeocoder = NodeGeocoder(osmOptions);
   },
   geocode: function(address){
     return new Promise((resolve, reject) => {
-      nodeGeocoder.geocode(address)
+      googleGeocoder.geocode(address)
         .then((res) => {
           console.info('Geocoding (' + address + ') : \n', res);
           resolve(res);
@@ -29,42 +37,35 @@ module.exports = {
 
   reverse: function(coordinates) {
     return new Promise((resolve, reject) => {
-      nodeGeocoder.reverse(coordinates)
+      googleGeocoder.reverse(coordinates)
         .then((res) => {
-          console.info('Reverse coordinates', JSON.stringify(coordinates), res);
+          console.info('Reverse coordinates w/ Google', JSON.stringify(coordinates), res);
           resolve(res);
         })
         .catch((err) => {
-          console.error('Failed to reverse coordinates', JSON.stringify(coordinates) , err);
-          reject(err);
+          if (err.message.includes('ZERO_RESULTS')){
+            console.info('Failed to reverse coordinates w/ Google - trying w/ OSM', JSON.stringify(coordinates));
+            osmGeocoder.reverse(coordinates)
+              .then((res) => {
+                console.info('Reverse coordinates w/ OSM', JSON.stringify(coordinates), res);
+                if (!res[0].city || !res[0].streetName){
+                  reject();
+                } else {
+                  res[0].formattedAddress = res[0].streetName + (res[0].streetNumber ? (' ' + res[0].streetNumber) : '') + ', ' + res[0].city;
+                  resolve(res);
+                }
+              })
+              .catch((err) => {
+                console.error('Failed to reverse w/ OSM', JSON.stringify(coordinates), err);
+                reject(err);
+              });
+          } else {
+            console.error('Failed to reverse coordinates w/ Google', JSON.stringify(coordinates), err);
+            reject(err);
+          }
         });
     });
   },
-
-  // geoReverse: function(coordinates) {
-  //   return new Promise((resolve, reject) => {
-  //     request({
-  //       uri: 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + coordinates.lon + ',' + coordinates.lat + ".json",
-  //       qs: {
-  //         access_token: _accessToken,
-  //         language: 'he'
-  //       },
-  //       method: 'GET'
-  //     }, function (error, response, body) {
-  //       if (!error && response.statusCode === 200) {
-  //         console.info('Reverse coordinates', coordinates, body);
-  //         const location = body.features[0];
-  //         const res = [{formattedAddress: location.place_name_he,
-  //           latitude:geometry.coordinates[1],
-  //           longitude:geometry.coordinates[0]}];
-  //         resolve(res);
-  //       } else {
-  //         console.error('Failed to reverse coordinates', coordinates, error, body);
-  //         reject(error);
-  //       }
-  //     });
-  //   });
-  // },
 
   verify: function(res) {
     if (res.length < 1){
