02100cd16f4c113caea20c6775229b542c2c36a5
yossipik <yossi.pik@gmail.com>
2017-12-08 15:36:57 +0200

    App - add support for 'car locked' case

--- app/src/constants/consts.js
+++ app/src/constants/consts.js
@@ -1,4 +1,15 @@
 export const InstanceTypes = {Sandbox: 'sandbox', Sandbox2: 'sandbox2', Production: 'production'};
 export const EventStatus = {Draft: 'draft', Submitted: 'submitted', Sent: 'sent', Assigned: 'assigned', Completed: 'completed', Archived: 'archived'};
 export const EventSource = {FB_BOT: 'fb-bot', App: 'app'};
-export const EventCases = ['כבלים', 'פנצ\'ר', 'קומפרסור', 'דלת נעולה', 'שמן\\מים\\דלק', 'חילוץ', 'קודנית', 'פנצ\'ר (אין רזרבי)', 'אחר'];
+export const EventCases = [
+  {case:0, label:'כבלים'},
+  {case:1, label:'פנצ\'ר'},
+  {case:9, label:'רכב נעול'},
+  {case:2, label:'קומפרסור'},
+  {case:4, label:'שמן\\מים\\דלק'},
+  {case:3, label:'דלת נעולה'},
+  {case:5, label:'חילוץ'},
+  {case:6, label:'קודנית'},
+  {case:7, label:'פנצ\'ר (אין רזרבי)'},
+  {case:8, label:'אחר'}
+  ];
