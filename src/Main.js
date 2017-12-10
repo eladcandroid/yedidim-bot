1d153e4d4b380e54f3841a49bff0b194846c605a
Alan Rubin <alan.rubin@keytree.co.uk>
2017-12-10 17:33:13 +0200

    Toast on mute/unmute

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 import AuthenticationScreen from 'screens/Authentication'
 import { inject, observer } from 'mobx-react/native'
-import { Root } from 'native-base'
-import { injectIntl } from 'react-intl'
+import { Root, Toast } from 'native-base'
+import { injectIntl, defineMessages } from 'react-intl'
 import AuthenticatedDrawer from 'layouts/AuthenticatedDrawer'
 
 @observer
 class Main extends Component {
+  componentDidUpdate(prevProps) {
+    const { isMuted } = this.props
+    if (isMuted !== prevProps.isMuted) {
+      // Muted value was changed, show toast
+      Toast.show({
+        text: isMuted
+          ? 'Notifications are muted for 24h'
+          : 'Notifications are now active',
+        position: 'bottom',
+        buttonText: 'OK',
+        duration: 10000,
+        type: isMuted ? 'danger' : 'success'
+      })
+    }
+  }
+
   render() {
     const { isAuthenticated, ...screenProps } = this.props
 
