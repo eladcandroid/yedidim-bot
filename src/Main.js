79db0cf34adc7ebfcdd100b1ee4c780169ac4ef4
Alan Rubin <alan.rubin@keytree.co.uk>
2017-12-10 17:37:20 +0200

    Adding intl to mute notifications

--- src/Main.js
+++ src/Main.js
@@ -5,18 +5,39 @@ import { Root, Toast } from 'native-base'
 import { injectIntl, defineMessages } from 'react-intl'
 import AuthenticatedDrawer from 'layouts/AuthenticatedDrawer'
 
+const mute = {
+  [true]: defineMessages({
+    text: {
+      id: 'Home.alert.mute.text',
+      defaultMessage: 'Notifications are muted for 24h'
+    },
+    buttonText: {
+      id: 'Home.alert.mute.buttonText',
+      defaultMessage: 'OK'
+    }
+  }),
+  [false]: defineMessages({
+    text: {
+      id: 'Home.alert.unmute.text',
+      defaultMessage: 'Notifications are now active'
+    },
+    buttonText: {
+      id: 'Home.alert.mute.buttonText',
+      defaultMessage: 'OK'
+    }
+  })
+}
+
 @observer
 class Main extends Component {
   componentDidUpdate(prevProps) {
-    const { isMuted } = this.props
+    const { isMuted, intl } = this.props
     if (isMuted !== prevProps.isMuted) {
       // Muted value was changed, show toast
       Toast.show({
-        text: isMuted
-          ? 'Notifications are muted for 24h'
-          : 'Notifications are now active',
+        text: intl.formatMessage(mute[isMuted].text),
         position: 'bottom',
-        buttonText: 'OK',
+        buttonText: intl.formatMessage(mute[isMuted].buttonText),
         duration: 10000,
         type: isMuted ? 'danger' : 'success'
       })
