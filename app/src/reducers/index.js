4e98fddb15cd5fc6f5637ad1b0bc6ab18ca87950
yossipik <yossi.pik@gmail.com>
2017-12-25 17:58:36 +0200

    App - Add user context and instance to Sentry

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
-import { dataSourceReducer } from "./dataSourceReducer";
+import { dataSourceReducer } from './dataSourceReducer';
+import { sentryReducer } from './sentryReducer';
 
 const reducers =
   combineReducers({
     dataSource: dataSourceReducer,
+    sentry: sentryReducer
   });
 
 export default reducers;
