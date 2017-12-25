4be8a4e8ccc029f037238876907ed8ccaedc2b97
yossipik <yossi.pik@gmail.com>
2017-12-25 17:58:36 +0200

    App - Add user context and instance to Sentry

--- app/src/app/App.js
+++ app/src/app/App.js
@@ -3,8 +3,12 @@ import { Provider } from 'react-redux';
 import Sentry from 'sentry-expo';
 import configureStore from '../store/configureStore';
 import HomeScreen from "../components/HomeScreen";
+import { getInstance } from "../common/utils";
+
+Sentry.enableInExpoDevelopment = true;
 
 Sentry.config('https://757d9938f3b64f20a85d536879e30e33@sentry.io/233665').install();
+Sentry.setTagsContext({"environment": getInstance()});
 
 const store = configureStore;
 
