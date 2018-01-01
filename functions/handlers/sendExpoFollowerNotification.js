c31efe6f7565bdd1efa95888c35d218ded0676af
Ygal Bellaiche <ygalbe@payoneer.com>
2018-01-01 15:58:25 +0200

    change to use expo-server-sdk

--- functions/handlers/sendExpoFollowerNotification.js
+++ functions/handlers/sendExpoFollowerNotification.js
@@ -1,61 +1,61 @@
-// TODO: use expo sdk
-const rp = require('request-promise');
+const Expo = require('expo-server-sdk');
+
+// Create a new Expo SDK client
+let expo = new Expo();
 
 exports.handleUpdateEvent = (event, admin) => {
-	let eventData = event.data.val();
-	console.log(' new is ' + eventData.status, 'event data ', eventData);
-	console.log(eventData);
+    let eventData = event.data.val();
+    console.log(' new is ' + eventData.status, 'event data ', eventData);
+    console.log(eventData);
 
-	// Get the list of device notification tokens.
-	const getDeviceTokensPromise = admin
-		.database()
-		.ref('/volunteer')
-		.orderByChild('NotificationToken')
-		.startAt('')
-		.once('value');
+    // Get the list of device notification tokens.
+    const getDeviceTokensPromise = admin
+        .database()
+        .ref('/volunteer')
+        .orderByChild('NotificationToken')
+        .startAt('')
+        .once('value');
 
-	// Get the follower profile.
-	return Promise.resolve(getDeviceTokensPromise).then(tokens => {
-		// Check if there are any device tokens.
-		if (!tokens.hasChildren()) {
-			return console.log('There are no notification tokens to send to.');
-		}
+    // Get the follower profile.
+    return Promise.resolve(getDeviceTokensPromise).then(tokens => {
+        // Check if there are any device tokens.
+        if (!tokens.hasChildren()) {
+            return console.log('There are no notification tokens to send to.');
+        }
 
-		console.log('There are', tokens.numChildren(), 'tokens to send notifications to.');
+        console.log('There are', tokens.numChildren(), 'tokens to send notifications to.');
 
-		console.log(tokens.val());
-		// Listing all tokens.
-		let tokenData = tokens.val();
-		const dataToSend = Object.keys(tokenData).map(function(t) {
-			let objectToSend = {};
-			objectToSend.to = tokenData[t].NotificationToken;
-			objectToSend.data = { key: eventData.key };
-			objectToSend.title = 'yedidim title';
-			objectToSend.body = eventData.details.address;
-			objectToSend.sound = 'default';
-			return objectToSend;
-		});
+        console.log(tokens.val());
+        // Listing all tokens.
+        let tokenData = tokens.val();
+        const dataToSend = Object.keys(tokenData).map(function(t) {
+            let objectToSend = {};
+            objectToSend.to = tokenData[t].NotificationToken;
+            objectToSend.data = {
+                key: eventData.key
+            };
+            objectToSend.title = 'yedidim title';
+            objectToSend.body = eventData;
+            objectToSend.sound = 'default';
+            return objectToSend;
+        });
 
-		console.log(dataToSend);
+        let chunks = expo.chunkPushNotifications(dataToSend);
+		let promises = [];
 
-		let options = {
-			method: 'POST',
-			uri: 'https://exp.host/--/api/v2/push/send',
-			body: dataToSend,
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			json: true // Automatically stringifies the body to JSON
-		};
-
-		return rp(options)
-			.then(function(parsedBody) {
-				console.log(parsedBody);
-				// POST succeeded...
-			})
-			.catch(function(err) {
-				console.log(err);
-				// POST failed...
+		for (let chunk of chunks) {
+			promises.push(new Promise(resolve => {
+			  expo.sendPushNotificationsAsync(chunk)
+				.then(receipts => {
+				  console.log("Successfully sent notifications : \n", receipts);
+				})
+				.catch(err => {
+				  console.error("Failed to send notifications : \n", err);
+				});
+			}));
+		  }
+		  return Promise.all(promises).then(() => {
+			  console.log('finish');
 			});
-	});
+    });
 };
