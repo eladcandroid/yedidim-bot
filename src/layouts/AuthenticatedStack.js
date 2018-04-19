5cd9e38262995442f6cc23e86b5399708f515e81
Dor Elmaliach <dor.elmal11@gmail.com>
2018-04-19 20:36:19 +0300

    help page with send test notification button

--- src/layouts/AuthenticatedStack.js
+++ src/layouts/AuthenticatedStack.js
@@ -4,12 +4,14 @@ import Home from '../screens/Home'
 import Event from '../screens/Event'
 import Feedback from '../screens/Feedback'
 import AboutStartach from '../screens/AboutStartach'
+import HelpPage from '../screens/HelpPage'
 
 const AuthenticatedStack = StackNavigator({
   Home: { screen: Home },
   Event: { screen: Event },
   Feedback: { screen: Feedback },
-  AboutStartach: { screen: AboutStartach }
+  AboutStartach: { screen: AboutStartach },
+  HelpPage: { screen: HelpPage }
 })
 
 export default withNotificationManager(AuthenticatedStack)
