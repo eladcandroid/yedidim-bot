706e891b6d26abe8c529df0d6013bf00dc268b5e
Alan Rubin <alan.rubin@keytree.co.uk>
2018-05-06 11:52:06 +0300

    Dealing correctly with errors on fetch request

--- volunteers-app/src/io/notifications.js
+++ volunteers-app/src/io/notifications.js
@@ -1,18 +1,29 @@
 import { firebaseFunctionsUrl } from 'config'
 import firebase from 'firebase'
 
-export const sendTestNotification = async (userId, role) =>
-  fetch(`${firebaseFunctionsUrl()}/sendTestNotification`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      userId,
-      role
-    })
-  })
+export const sendTestNotification = async (userId, role) => {
+  const response = await fetch(
+    `${firebaseFunctionsUrl()}/sendTestNotification`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        userId,
+        role
+      })
+    }
+  )
+
+  if (response.status === 200) {
+    return true
+  }
+
+  const errorMessage = await response.text()
+  throw new Error(errorMessage)
+}
 
 export const acknowledgeTestNotification = async userId =>
   firebase
