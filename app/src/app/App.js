f3db31a6b2335d6708cb061f03b8b1651417bb87
Dor Elmaliach <dor.elmal11@gmail.com>
2018-05-16 20:34:45 +0300

    adding one signal to dispatchers

--- app/src/app/App.js
+++ app/src/app/App.js
@@ -4,6 +4,7 @@ import Sentry from 'sentry-expo';
 import configureStore from '../store/configureStore';
 import HomeScreen from "../components/HomeScreen";
 import { getInstance } from "../common/utils";
+import OneSignal from "react-native-onesignal";
 
 Sentry.config('https://757d9938f3b64f20a85d536879e30e33@sentry.io/233665').install();
 Sentry.setTagsContext({'environment': getInstance(), 'version': Expo.Constants.manifest.version});
@@ -13,11 +14,38 @@ const store = configureStore;
 class App extends Component {
 
   async componentWillMount() {
+
+    OneSignal.init("2ee2bdaa-39dc-4667-99d6-e66e81af79ba");
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+
     await Expo.Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'), // eslint-disable-line
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'), // eslint-disable-line
       Ionicons: require('native-base/Fonts/Ionicons.ttf') // eslint-disable-line
     })
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
   }
 
   render() {
