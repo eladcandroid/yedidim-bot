aaa8cc5bbe505e25cdb04bb973dbad30cd2341c0
Alan Rubin <alan.rubin@keytree.co.uk>
2018-06-06 10:50:26 +0300

    Notification test

--- volunteers-app/notification-test.sh
+++ volunteers-app/notification-test.sh
@@ -1,9 +1,9 @@
-curl -H "Content-Type: application/json" -X POST https://exp.host/--/api/v2/push/send -d '{
-  "to": "ExponentPushToken[WZ8mlOCmbuW_kyzp1tbIOY]",
-  "title":"אירוע חדש",
+curl -H "Content-Type: application/json; charset=utf-8" -X POST https://onesignal.com/api/v1/notifications -d '{
+  "app_id": "e5ef1cdc-a50b-430f-8fac-b7702740c59a",
+  "include_player_ids": ["a19ce0e9-b13d-41e0-8450-71f81d42581e"],
+  "headings": {"en": "אירוע חדש"},
+  "contents": {"en": "היי אלן, אתה נדרש בקריאה מסוג פונצ׳ר דרך הנשיאים 45 י-ם. לחץ לפרטים נוספים"},
   "data": {
-      "key": "-L1y19rZZGK6p91r9ByU"
-  },
-  "body": "היי אלן, אתה נדרש בקריאה מסוג פונצ׳ר דרך הנשיאים 45 י-ם. לחץ לפרטים נוספים",
-  "sound": "default"
+      "key": "-LDbbpzvdZWxljGGYbWO"
+  }
 }'
