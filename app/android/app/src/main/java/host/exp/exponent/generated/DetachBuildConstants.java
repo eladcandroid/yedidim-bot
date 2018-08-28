2866dddcec3fbc83645d0cd6e17a065feb2c7b0b
Alan Rubin <alan.rubin@keytree.co.uk>
2018-08-28 19:15:42 +0300

    Ignore changed files

--- app/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "expf2d929db42794c5ca64e076d905d21d0://192.168.14.163:80";
-
-}
