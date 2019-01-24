af671283899e9ad74555467d23cb610a5c3800b0
Yogev <Yogev@twist>
2019-01-24 05:13:37 +0200

    Show location list, Edit city

--- volunteers-app/src/screens/MyCities.js
+++ volunteers-app/src/screens/MyCities.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
-import appStyles from '../global-styles'
-import styled from 'styled-components/native'
-import { FormattedMessage } from 'react-intl'
-import { I18nManager, Linking, Image, View } from 'react-native'
+import { FormattedMessage, FormattedRelative } from 'react-intl'
+import {
+  I18nManager,
+  Linking,
+  Image,
+  View,
+  ActivityIndicator
+} from 'react-native'
 import { trackEvent } from 'io/analytics'
 import StartachLogo from 'images/startach-logo.jpg'
 import AlignedText from 'components/AlignedText'
@@ -20,16 +24,77 @@ import {
   Text,
   Grid,
   Col,
-  Row
+  Row,
+  List,
+  ListItem
 } from 'native-base'
 import { NavigationActions } from 'react-navigation'
-import { ListItem } from './SideBar'
+import AlignedText from 'components/AlignedText'
+import debounce from "lodash.debounce";
 
 const MarginView = styled.View`
   margin: 10px 10px;
 `
 const zeroStateText = 'קבלת התראות מישובים קבועים, גם כשאינך בקרבת מקום'
 
+const LocationItem = observer(
+  ({
+    onPress,
+    location: {
+        id,
+        name,
+        lat,
+        lon,
+        isLoading
+    }
+  }) =>
+    isLoading ? (
+      <ListItem avatar>
+        <Left>
+          <ActivityIndicator size="large" />
+        </Left>
+        <Body>
+          <FormattedMessage
+            id="Home.event.loadingTitle"
+            defaultMessage="Please wait, loading event..."
+          >
+            {txt => <AlignedText>{txt}</AlignedText>}
+          </FormattedMessage>
+          <AlignedText note />
+        </Body>
+        <Right />
+      </ListItem>
+    ) : (
+      <ListItem
+        avatar
+        onPress={() => {
+          onPress(location)
+        }}
+        style={{
+          width: '90%',
+          backgroundColor: 'white',
+          borderBottomWidth: 3,
+          borderBottomColor: 'red',
+          marginTop: 10,
+          marginRight: 'auto',
+          marginBottom: 0,
+          marginLeft: 'auto'
+        }}
+      >
+        <Body>
+          <AlignedText>
+            <Text style={{ fontWeight: 'bold' }}>
+              {' '}
+              {name} {'\n'}{' '}
+            </Text>{' '}
+          </AlignedText>
+        </Body>
+      </ListItem>
+    )
+)
+
+
+@observer
 class MyCities extends Component {
   static navigationOptions = ({ navigation }) => ({
     header: (
@@ -65,11 +130,32 @@ class MyCities extends Component {
     )
   })
 
-  render() {
+    handleLocationItemPress = debounce(
+        location => {
+            trackEvent('Navigation', { page: 'EditCity', location })
+            this.props.navigation.navigate('EditCity', { location:location })
+        },
+        1000,
+        { leading: true, trailing: false }
+    )
+
+
+    render() {
     return (
       <Container>
         <Content style={{ flex: 1, backgroundColor: '#fff' }}>
-          <AlignedText>{zeroStateText}</AlignedText>
+          <View style={{ alignItems: 'center', marginTop: 30 }}>
+            <LabelText>{zeroStateText}</LabelText>
+            {this.props.currentUser.locations.map(location => {
+              console.log('!!!!!', location)
+                return <LocationItem
+                    location={location}
+                    onPress={this.handleLocationItemPress}
+
+                />
+            })}
+          </View>
+
           <Button
             style={{ width: 100, height: 20 }}
             onPress={() => {
@@ -86,4 +172,6 @@ class MyCities extends Component {
   }
 }
 
-export default MyCities
+export default inject(({ stores }) => ({
+  currentUser: stores.authStore.currentUser
+}))(MyCities)
